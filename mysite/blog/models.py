6673f25c9312da6c31bc152d0c95c43efcc92de4
Peizhe Zhang <zjuzpz@gmail.com>
2016-05-26 12:07:47 -0700

    add post/post detail page

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.core.urlresolvers import reverse
+#Create your models here.
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -28,6 +29,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args = [self.publish.year,
+                                self.publish.strftime('%m'),
+                                self.publish.strftime('%d'),
+                                self.slug])
 
 
 
